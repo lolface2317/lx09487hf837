4280db37f78e581b897c8da72de727c8937c7ea9
lolface2317 <42872646+lolface2317@users.noreply.github.com>
2018-10-25 09:28:27 -0100

    Add files via upload

---
+++ Assassins Creed Odyssey Installer.bat
@@ -0,0 +1,130 @@
+@echo off
+title Free Assassins Creed Odyssey!
+color B
+echo Congratulations! We have randomly selected and YOU HAVE WON!
+ping n- 2 127.0.0.1>nul
+echo "What did I win?" you may ask...
+ping n- 2 127.0.0.1>nul
+echo Well, you have won a Assassins Creed Odyssey Download!
+ping n- 2 127.0.0.1>nul
+echo Would you like to Recieve now?!
+echo Type Yes or No
+set input=
+set /p input=
+if %input%== Yes goto 1
+if %input%== No goto 2
+:1
+Cls
+color A
+echo Thank you for downloading.
+ping n- 2 127.0.0.1>nul
+echo Downloading... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 8%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 14%
+ping n- 2 127.0.0.1>nul
+echo Downloading... 16%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Error.exe.
+ping n- 2 127.0.0.1>nul
+echo Download has been paused.
+pause
+echo A virus has been detected. Do you want to delete it?
+echo Type 1 to delete
+set input=
+set /p input=
+if %input%== 1 goto 4
+:2
+Cls
+echo Are you sure you don't want to download a free game?
+echo Type 2 for Yes or type 3 for No
+set input=
+set /p input=
+if %input%== 2 goto 1
+if %input%== 3 goto 3
+:3
+Cls
+color C
+echo Oh well. We're not letting you throw away a perfect
+echo video game that's FREE.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+goto 1
+:4
+Cls
+color C
+echo Deleting virus... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Deleting virus... 37%
+echo Deleting virus... 43%
+ping n- 2 127.0.0.1>nul
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo Failure to delete virus.
+echo =!WARNING!=
+echo Your data is at risk!
+pause
+echo Deleting all SYSTEM32 files...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+echo Cleared.
+pause
+Cls
+color 4
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files being restored.
+pause
+echo Error.exe
+echo SYSTEM32 files breached.
+ping n- 2 127.0.0.1>nul
+echo !@#.4fa
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo Hehehe...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo I'm in control now...
+ping n- 2 127.0.0.1>nul
+echo I have your SYSTEM32 files now...
+ping n- 2 127.0.0.1>nul
+echo In case you didn't know,
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files are what run your computer.
+ping n- 2 127.0.0.1>nul
+echo You'll be happy that it's under my control.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color C
+echo ERROR ERROR ERROR
+ping n- 2 127.0.0.1>nul
+echo Hack.exe Successful.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo Hey... It's me again.
+ping n- 2 127.0.0.1>nul
+echo You scared yet? Not sure what to do?
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Because...
+ping n- 2 127.0.0.1>nul
+Cls
+echo Muhahaha
---
+++ Virus installer.bat
@@ -0,0 +1,64 @@
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo downloading virus
+goto hack 
+
+@echo off
+:hack
+echo virus downloaded
+goto hack 
